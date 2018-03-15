ab0c61a8c8f938e2cc617d975ae27b001ba036c6
Cireid <gabrieldieric@gmail.com>
2018-03-15 10:56:58 -0300

    primeiro commit

---
+++ src/main/java/com/aulapablo/p5String2/P5AulaSpringboot2Application.java
@@ -0,0 +1,12 @@
+package com.aulapablo.p5String2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5AulaSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5AulaSpringboot2Application.class, args);
+	}
+}
