7e23436e554962f004bb844166775e41b60b19e7
shennhak <elizabeth.albuquelima@gmail.com>
2018-03-15 11:28:21 -0300

    2commit

--- src/main/java/com/aulapablo/p5String2/P5AulaSpringboot2Application.java
+++ src/main/java/com/aulapablo/p5String2/P5AulaSpringboot2Application.java
@@ -3,6 +3,8 @@ package com.aulapablo.p5String2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aulapablo.p5String2.resources.CategoriaResource;
+
 @SpringBootApplication
 public class P5AulaSpringboot2Application {
 
